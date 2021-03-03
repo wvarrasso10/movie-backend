26606082544e6a81508c38f58caaa9c7bc1cceb5
wesley varrasso <wvarrasso10@gmail.com>
2021-03-03 13:04:54 -0800

    first commit

---
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using DemoApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication2.Data
+{
+    //class that defines how we talk to database
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //need db set for model to turn into table and communicate
+        //movie class is single entity or table
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
---
+++ WebApplication2/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        [Column(TypeName= "decimal(10,2)")]
+        [DataType(DataType.Currency)]
+        //[Range(1,100)]
+        public decimal Price { get; set; }
+    }
+}
